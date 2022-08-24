603db50a4d173352d417df676f5d6d73ec39aab1
Grantley Cullar <83633399+gocrazygt@users.noreply.github.com>
2022-08-24 18:36:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
+# You can use this in a video file by removing 0 (default camera) and replacing it with the path of your video
 video = cv2.VideoCapture(0)
 
 while True:
