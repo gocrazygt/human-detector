ee59a2e1596ea7828519a30ae7d1de0dbc56e58d
Grantley Cullar <83633399+gocrazygt@users.noreply.github.com>
2022-08-24 18:30:56 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+video = cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    eyes = eye_cascade.detectMultiScale(
+        frame, 
+        scaleFactor = 1.2,
+        minNeighbors = 5
+    )
+
+    faces = face_cascade.detectMultiScale(
+        frame,
+        scaleFactor = 1.2, 
+        minNeighbors = 5
+    )
+
+    for (x,y,w,h) in eyes:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0, 255, 0),2)
+
+    for x,y,w,h in faces:
+        frame = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+
+    cv2.imshow('Human Detector', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
